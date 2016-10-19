90ece77baa7da6b05abf27ad98eaa5c5fae0490f
TahaniKarroum <tahani.karroum@gmail.com>
2016-10-19 22:51:15 +0300

    Add client information when he submit order

--- app/src/main/java/com/example/pc/restoapplication/Cart/CartFragment.java
+++ app/src/main/java/com/example/pc/restoapplication/Cart/CartFragment.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -38,6 +39,7 @@ public class CartFragment extends Fragment implements OnItemClickListener<Order_
 
     RelativeLayout ll;
     ListView list;
+    Button confirm;
     private ArrayList<Order_Product> order_products;
     private CartListViewAdapter mAdapter;
     private ProgressDialog nDialog;
@@ -57,13 +59,55 @@ public class CartFragment extends Fragment implements OnItemClickListener<Order_
         mAdapter = new CartListViewAdapter();
         mAdapter.setOnItemClickListener(this);
         mAdapter.setOnLongClickListener(this);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cart, container, false);
         list = (ListView) view.findViewById(R.id.list);
+        confirm = (Button) view.findViewById(R.id.confirm);
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = mainActivity.getLayoutInflater();
+                View alertLayout = inflater.inflate(R.layout.information, null);
+                final EditText nameedittext = (EditText) alertLayout.findViewById(R.id.name);
+                final EditText phoneedittext = (EditText) alertLayout.findViewById(R.id.phone);
+                final EditText addressedittext = (EditText) alertLayout.findViewById(R.id.address);
+                AlertDialog.Builder alert = new AlertDialog.Builder(context);
+                alert.setTitle("Set Quantity");
+                // this is set the view from XML inside AlertDialog
+                alert.setView(alertLayout);
+                // disallow cancel of AlertDialog on click of back button and outside touch
+                alert.setCancelable(false);
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(context, "Cancel clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String namestr = nameedittext.getText().toString();
+                        String phonestr = phoneedittext.getText().toString();
+                        String addressstr = addressedittext.getText().toString();
+                        if(namestr.length()==0 || phonestr.length()==0|| addressstr.length()==0){
+                            Toast.makeText(context, "You should enter name , address and phone !", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            //Toast.makeText(context, "Name: " + namestr, Toast.LENGTH_SHORT).show();
+                            fillClientInformation(namestr, phonestr, addressstr);
+                            //Toast.makeText(context, "Phone: " + phonestr, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                AlertDialog dialog = alert.create();
+                dialog.show();
+            }
+        });
         Log.i("iii", "onCreateView: ");
         order_products = new ArrayList<Order_Product>();
         try {
@@ -157,8 +201,6 @@ public class CartFragment extends Fragment implements OnItemClickListener<Order_
     @Override
     public void onLongClick(View v, int position, final Order_Product order_product) {
         Log.i("Product fragment", " dhdhe" + order_product.getName());
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
-        final EditText et = new EditText(context);
         LayoutInflater inflater = mainActivity.getLayoutInflater();
         View alertLayout = inflater.inflate(R.layout.layout_custom_dialog, null);
         final EditText qtyedittext = (EditText) alertLayout.findViewById(R.id.qty);
@@ -215,6 +257,27 @@ public class CartFragment extends Fragment implements OnItemClickListener<Order_
                     nDialog.dismiss();
                     e.printStackTrace();
                 }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.i("failureeeee", " two");
+                nDialog.dismiss();
+            }
+        });
+    }
+
+    public void fillClientInformation(String name, String phone, String address) {
+        name=name.trim();
+        phone=phone.trim();
+        address=address.trim();
+        Log.i("fffff","ffff "+name+" "+phone+" "+address);
+        nDialog = ProgressDialog.show(getActivity(), "Loading...", "Please wait...", true);
+        String functionName = "fillClientInformation?deviceid=" + mainActivity.android_id +"&name=" + name + "&address=" + address + "&phone=" + phone;
+        CommunicationAsyn.getWithoutParams(functionName, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                nDialog.dismiss();
             }
 
             @Override
